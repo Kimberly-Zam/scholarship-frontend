35f9372527760c10c9c07dd29889026a6172264f
Kimberly-Zam <kimberlyzambrana41@gmail.com>
2025-06-29 06:05:14 -0400

    Desarrollo de las Cards

--- src/app/pages/scholarships/scholarships.component.ts
+++ src/app/pages/scholarships/scholarships.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
+import { CardsComponent } from "../../cards/cards.component";
 
 @Component({
   selector: 'app-scholarships',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CardsComponent],
   templateUrl: './scholarships.component.html',
   styleUrl: './scholarships.component.scss'
 })
